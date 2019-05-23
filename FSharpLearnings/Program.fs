07e5c57d7036409e49876b55dfdd4b69e4dbf51f
Fredrik Sandström <veckodag@live.com>
2019-05-23 15:45:14 +0200

    More happy F# stuff!

--- FSharpLearnings/Program.fs
+++ FSharpLearnings/Program.fs
@@ -83,19 +83,147 @@ open System
 
 //do_math()
 
-let string_stuff() = 
+//let string_stuff() = 
 
-    let str1 = "This is an ordinary string!"
-    let str2 = @"Verbatim sting, ignores backslashes \ \\"
-    let str3 = """ "I Ignore double quoutes " and backslashes \ \\" """
+//    let str1 = "This is an ordinary string!"
+//    let str2 = @"Verbatim sting, ignores backslashes \ \\"
+//    let str3 = """ "I Ignore double quoutes " and backslashes \ \\" """
 
-    let str4 = str1 + " " + str2    
+//    let str4 = str1 + " " + str2    
 
-    printfn "Length : %i" (String.length str4)
-    printfn "%c" str3.[4]
-    printfn "1st word : %s" (str2.[0..7])
+//    printfn "Length : %i" (String.length str4)
+//    printfn "%c" str3.[4]
+//    printfn "1st word : %s" (str2.[0..7])
 
-string_stuff()
+//    let upper_str = String.collect (fun c -> sprintf"%c, " c) "commas"
+//    printfn "Commas : %s" upper_str
+//    printfn "Any upper : %b" (String.exists (fun c -> Char.IsUpper(c)) str2)
+//    printfn "Number : %b" (String.forall (fun c -> Char.IsDigit(c)) "1234")
+
+//    let string1 = String.init 10 (fun i -> i.ToString())
+//    printfn "More Numbers : %s" string1
+
+//    String.iter(fun c -> printfn "%c" c) "Print Me!"
+
+//string_stuff()
+
+//let loop_stuff() =
+//    let magic_num = "7"
+//    let mutable guess = ""
+
+//    while not (magic_num.Equals(guess)) do
+//        printf "Guess the number : "
+//        guess <- Console.ReadLine()
+
+//    printfn "You guessed the Magic Number!"
+
+//loop_stuff()
+
+//let more_loops() = 
+//    for i = 1 to 10 do 
+//        printfn "Up : %i" i
+
+//    for i = 10 downto 1 do
+//        printfn "Down : %i" i
+
+//    for i in [1..10] do
+//        printfn "Range : %i" i
+
+//    [1..10] |> List.iter (printfn "Num : %i")
+
+//    let sum = List.reduce (+) [1..10]
+//    printfn "Sum : %i" sum
+
+//more_loops()
+
+//let cond_stuff() = 
+//    let age = 8
+
+//    if age < 5 then
+//        printfn "Preschool"
+//    elif age = 5 then
+//        printfn "Kindergarten"
+//    elif (age > 5) && (age <= 18) then
+//        let grade = age - 5
+//        printfn "Go to Grade %i" grade
+//    else 
+//        printfn "Go to College"
+
+//    let gpa = 3.9
+//    let income = 15000
+
+//    printfn "College Grant : %b" ((gpa >= 3.8) || (income <= 12000))
+//    printfn "Not True : %b" (not true)
+
+//    let grade2: string = 
+//        match age with
+//        | age when age < 5 -> "Preschool"
+//        | 5 -> "Kindergarten"
+//        | age when (age > 5) && (age <= 18) -> (age - 5).ToString()
+//        | _ -> "College"
+
+//    printfn "Grade2 : %s" grade2    
+
+//cond_stuff()
+
+//let list_stuff() = 
+//    let list1 = [1;2;3;4]
+
+//    list1 |> List.iter (printfn "Num : %i")
+//    printfn "%A" list1
+
+//    let list2 = 5::6::7::[]
+//    printfn "%A" list2
+
+//    let list3 = [3..6]
+//    let list4 = ['a'..'g']
+//    printfn "%A" list4
+
+//    let list5 = List.init 5 (fun i -> i * 2)
+//    printfn "%A" list5
+
+//    printfn "COOL STUFF BELOW!"
+//    let list6 = [ for a in 1..5 do yield (a * a) ]
+//    printfn "%A" list6
+//    let list7 = [ for a in 1..20 do if a % 2 = 0 then yield a ]
+//    printfn "%A" list7
+//    let list8 = [ for a in 1..3 do yield! [a .. a + 2]]
+//    printfn "%A" list8
+
+//    printfn "Length : %i" list8.Length
+//    printfn "Empty : %b" list8.IsEmpty
+//    printfn "At index : %c" (list4.Item(2))
+//    printfn "Head : %c" (list4.Head)
+//    printfn "Tail : %A" (list4.Tail)
+
+//    let list9 = list3 |> List.filter (fun x -> x % 2 = 0)
+
+//    let list10 = list9 |> List.map (fun x -> (x * x))
+
+//    printfn "Filtered : %A" list9
+//    printfn "Mapped: %A" list10
+//    printfn "Sorted : %A" (List.sort [1;5;4;3;2])
+//    printfn "Sum : %i" (List.fold (fun sum elem -> sum + elem) 0 [1;2;3])
+
+//list_stuff()
+
+type emotion = 
+| joy = 0
+| fear = 1
+| anger = 2
+
+let enum_stuff() = 
+    let my_feeling = emotion.joy
+
+    let current_emotion = match my_feeling with
+    | emotion.joy ->"I'm joyful"
+    | emotion.fear -> "I'm fearful"
+    | emotion.anger -> "I'm angry"
+    | _ -> "I'm emotionless"
+
+    printfn "%s" current_emotion
+
+enum_stuff()
 
 Console.ReadKey () |> ignore
     
